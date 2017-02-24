f1b742b5d19ccf2191686e50c11527d95d4f8d22
Dmitry Matveev <ritmix@soft-way.biz>
2017-02-24 20:55:07 +0300

    Add alien fleet moving

--- game_functions.py
+++ game_functions.py
@@ -49,6 +49,24 @@ def update_bullets(bullets):
             bullets.remove(bullet)
 
 
+def check_fleet_edges(ai_settings, aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+
 def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
