a3e4ea8adbbf98cab76e6775ebe264147504062c
Dmitry Matveev <dmatveev@kupo.la>
2017-03-06 14:17:25 +0300

    Add high score

--- game_functions.py
+++ game_functions.py
@@ -96,6 +96,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens,
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
         sb.prep_score()
+        check_high_score(stats, sb)
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
@@ -185,3 +186,8 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         pygame.mouse.set_visible(True)
         stats.game_active = False
 
+
+def check_high_score(stats, sb):
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
